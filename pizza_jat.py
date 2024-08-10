ef1c6b315ffd9f6b142a587d7142b991afe9a869
Fernando <fbanarescarrasco@gmail.com>
2024-08-10 15:16:29 -0400

    segundo borron

--- pizza_jat.py
+++ pizza_jat.py
@@ -85,8 +85,8 @@ def seleccionar_ingredientes():
                 print(f"{ingrediente.capitalize()} no está en tu pizza.")
 
 def calcular_tiempo_preparacion():
-    tiempo_base = 20  # 20 minutos base
-    tiempo_por_ingrediente = 2 * len(ingredientes_seleccionados)  # 2 minutos por cada ingrediente
+    tiempo_base = 20
+    tiempo_por_ingrediente = 2 * len(ingredientes_seleccionados)
     tiempo_total = tiempo_base + tiempo_por_ingrediente
     return tiempo_total
 
@@ -103,7 +103,6 @@ def confirmar_orden():
         else:
             print("Respuesta no válida, por favor ingrese 'sí' o 'no'.")
 
-# Ejecución del programa
 preparar_pizza()
 seleccionar_ingredientes()
 tiempo_preparacion = calcular_tiempo_preparacion()
